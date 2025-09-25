e1b205efc3d953992288935994c427047d2a9501
Bas van den Heuvel <bas.van.den.heuvel@philips.com>
2025-09-25 14:01:57 +0200

    Changed valueset filenname to just id to remove duplicates.

--- runSushiAndBuildIg.sh
+++ runSushiAndBuildIg.sh
@@ -4,6 +4,7 @@ rm input/resources/*
 cp -R input-src/* input/
 sushi .
 cp fsh-generated/resources/* input/resources
-rm input/resources/ImplementationGuide*.json
+rm -Rf fsh-generated/resources/*
+# rm input/resources/ImplementationGuide*.json
 rm input/fsh/*
-./_genonce.sh -no-sushi
+./_genonce.sh
